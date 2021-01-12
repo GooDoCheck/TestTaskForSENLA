1d27e45df6e69e3f1690c659d34015bc36457536
GooDoCheck <ilya-odin16@mail.ru>
2021-01-13 00:30:54 +0300

    Entered main code program

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class TaskOne {
+    public static boolean integerCheck = false;
+    public static float numberFloat =  0;
+    public static int numberInteger = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        numberFloat = scanner.nextFloat();
+        integerNumberCheck(numberFloat);
+        if (integerCheck){
+            System.out.println("Число " + numberInteger + " - " + positiveNumberCheck(numberInteger) + primeNumberCheck(numberInteger));
+        }
     }
+
+    public static void integerNumberCheck(float number) {
+        if (number % 1 == 0) {
+            numberInteger = (int) number;
+            integerCheck = true;
+        } else {
+            System.out.println("Ошибка! Введено не целое число");
+        }
+
+    }
+
+    public static String positiveNumberCheck (int checkNum) {
+        if (checkNum % 2 == 0)
+            return " четное";
+        else
+            return " нечетное";
+    }
+
+    public static String primeNumberCheck (int checkNum) {
+        int temp;
+        boolean isPrime = true;
+        for (int i=2; i<=checkNum/2; i++) {
+            temp = checkNum % i;
+            if (temp == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) {
+            return  " простое число.";
+        } else {
+            return " составное число.";
+        }
+    }
+
 }
